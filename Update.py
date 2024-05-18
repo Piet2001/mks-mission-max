f4bb4ea3d6b7c5b823ed1eb5f939e9bebe5e19d1
Piet2001 <piet2001@fireman.net>
2024-05-18 23:36:27 +0200

    fix wrong key

--- Update.py
+++ Update.py
@@ -44,15 +44,15 @@ for i in data:
                     else:
                         i["requirements"][j] = k["requirements"][j]
                 elif (j == "personnel_educations"):
-                    for m in k["additional"]["personnel_educations"]:
-                        if not("personnel_educations" in i["additional"]):
-                            i["additional"]["personnel_educations"] = {}
-                        if(isinstance(k["additional"]["personnel_educations"][m], int)):
-                            if(m in i["additional"]["personnel_educations"]):
-                                if(k["additional"]["personnel_educations"][m] > i["additional"]["personnel_educations"][m]):
-                                    i["additional"]["personnel_educations"][m] = k["additional"]["personnel_educations"][m]
+                    for m in k["requirements"]["personnel_educations"]:
+                        if not("personnel_educations" in i["requirements"]):
+                            i["requirements"]["personnel_educations"] = {}
+                        if(isinstance(k["requirements"]["personnel_educations"][m], int)):
+                            if(m in i["requirements"]["personnel_educations"]):
+                                if(k["requirements"]["personnel_educations"][m] > i["requirements"]["personnel_educations"][m]):
+                                    i["requirements"]["personnel_educations"][m] = k["requirements"]["personnel_educations"][m]
                             else:
-                                i["additional"]["personnel_educations"][m] = k["additional"]["personnel_educations"][m]
+                                i["requirements"]["personnel_educations"][m] = k["requirements"]["personnel_educations"][m]
             for l in k["additional"]:
                 if(isinstance(k["additional"][l], int)):
                     if(l in i["additional"]):
